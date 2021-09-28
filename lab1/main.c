83df80c3bb7a69d4c4e01a929e2a8798c6c87c9a
Lokol1337 <drejfeld@gmail.com>
2021-09-28 10:03:30 +0300

    lab1

---
+++ lab1/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+void function(){
+	printf("atexit отработал -- %d -- %d\n",getpid(), getppid());
+}
+
+int main(int argc, char** argv){
+	pid_t pid;
+	pid = fork();
+	int status;
+	atexit(function);
+	switch(pid){
+		case 0: printf("Я: %d\nМой родитель:%d\n",getpid(), getppid()); break;
+		case -1: printf("Я ошибка\n"); break;
+		default: printf("Родитель: %d\nМой родитель:%d\n", getpid(),getppid()); wait(&status); printf("Cтатус ребёнка: %d\n",WEXITSTATUS(status)); break;
+	}
+	
+	return 0;
+
+}
