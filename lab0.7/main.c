7272bd5f391a69b427119ac1ba28190360153e08
Lokol1337 <drejfeld@gmail.com>
2021-10-12 20:26:33 +0300

    Архиватор

---
+++ lab0.7/main.c
@@ -0,0 +1,214 @@
+#include <stdio.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+void input(char* pathArch,char* pathFile){
+	FILE *arch, *file;
+	
+	long long int sizeFile = 0;
+	char *buf = NULL;
+	struct stat statFile;
+	mode_t curmode;
+	if(((arch = fopen(pathArch, "a")) != NULL)){
+		if(((file = fopen(pathFile, "r")) != NULL)){
+			fseek (file, 0, SEEK_END);
+			sizeFile = ftell(file);
+			fseek (file, 0, SEEK_SET);
+			buf = malloc (sizeFile);
+			if(buf)
+			{
+				fread (buf, 1, sizeFile, file);
+			}
+		
+			stat(pathFile, &statFile);
+			
+			fprintf(arch, "%d\n", strlen(pathFile));
+			fprintf(arch, "%s\n", pathFile);
+			fprintf(arch, "%d\n", statFile.st_mode);
+			fprintf(arch, "%d\n", sizeFile);
+			fprintf(arch, "%s", buf);
+
+			fclose (file);	
+		}
+		fclose(arch);
+	}
+	
+	else
+		exit(1);
+}
+
+void extract(char* pathArch, char* pathFile){
+	FILE *arch, *file;
+	char* after = 0;
+	char* before = 0;
+	long long int beforeSize = 0;
+	long long int afterSize = 0;
+	long long int fileSize = 0;
+	long long int delSize = 0;
+
+	mode_t bufMode;
+	long long int bufNameSize = 0;
+	char* bufName;
+	long long int bufConSize = 0;
+	char* bufCon;
+	int flag = 0;
+	if((arch = fopen(pathArch, "r")) != NULL){
+		fseek (arch, 0, SEEK_END);
+		fileSize = ftell(arch);
+		fseek (arch, 0, SEEK_SET);
+		while(!feof(arch)){
+			if((fscanf(arch, "%d", &bufNameSize)) == EOF){
+				break;
+			}
+			beforeSize = ftell(arch);
+			fgetc(arch);
+			bufName = malloc(bufNameSize);
+			fread(bufName, 1, bufNameSize, arch);
+			fgetc(arch);		
+                         
+			fscanf(arch, "%d", &bufMode);
+			fgetc(arch);          
+			fscanf(arch, "%d", &bufConSize);
+			fgetc(arch);
+			bufCon = malloc(bufConSize);
+			fread(bufCon, 1, bufConSize, arch);
+			if(strcmp(bufName,pathFile) == 0){
+				file = fopen(pathFile, "w");
+				chmod(pathFile, bufMode);
+				fprintf(file, "%s", bufCon);
+				flag = 1;
+				fclose(file);
+				break;
+			}
+			bufNameSize = 0;
+			bufMode = 0;
+			bufConSize = 0;
+			bufName = 0;
+			
+		}
+		
+		if(flag == 1){
+			afterSize = fileSize - ftell(arch);
+			delSize = ftell(arch) - beforeSize;
+		}
+		else{
+			fclose(arch);
+			exit(1);
+		}
+		
+	}
+
+	fseek(arch, 0, SEEK_SET);
+	
+	
+	if(beforeSize != 0){
+		before = malloc(beforeSize);	
+		fread(before, 1, beforeSize, arch);
+
+	}
+
+	for(int i = 0; i < delSize; i++){
+		fgetc(arch);
+	}
+	if(afterSize != 0){
+		after = malloc(afterSize);
+		fread(after, 1, afterSize, arch);
+	}
+	
+	fclose(arch);
+	
+	if((arch = fopen(pathArch, "w")) != NULL){
+		
+		if(before){
+			fwrite(before, beforeSize - 1, 1, arch);
+		}
+		if(after){;
+			fwrite(after, afterSize, 1, arch);
+		}
+		fclose(arch);
+	}
+	
+	
+	
+	
+	
+}
+
+void stats(char* pathArch){
+	FILE* arch;
+	long long int bufCap = 0;
+	int countAll = 0;
+	long long int bufNameSize = 0;
+	long long int bufCount = 0;
+	long long int bufConSize = 0;
+	mode_t bufMode;
+	char* bufCon;
+	char* bufName;
+	if(((arch = fopen(pathArch, "r")) !=  NULL)){
+		printf("Файлы в архиве: %s\n", pathArch);
+		while(!feof(arch)){
+			fscanf(arch,"%d",&bufNameSize);
+			fgetc(arch);
+			bufName = malloc(bufNameSize);	
+			fread(bufName, 1, bufNameSize, arch);
+			fgetc(arch);
+			fscanf(arch, "%d", &bufMode);
+			fgetc(arch);
+			fscanf(arch, "%d", &bufConSize);
+			fgetc(arch);
+			bufCon = malloc(bufConSize);
+			fread(bufCon, 1, bufConSize, arch);
+			if(bufNameSize){
+                                bufCount++;
+				printf("%d. %s\n",bufCount, bufName);
+				bufCap += bufConSize;
+                        }
+			bufConSize = 0;
+			bufNameSize = 0;
+			bufMode = 0;
+			bufName = 0;
+		}
+		fclose(arch);
+		printf("Всего файлов: %d\nС общим весом: %d\n", bufCount, bufCap);
+
+	}
+}
+
+void help(){
+	printf("Доступные команды:\n-i: ./main arch -i test.txt\n-e: ./main arch -e test.txt\n-s: ./main arch -s\n");
+}
+
+int main(int argc, char** argv){
+	int result = 0;
+	char* arch;
+	
+	if(argc > 1){
+		if(argv[1][0] != '-')
+			arch = argv[1];
+	}
+	while((result = getopt(argc, argv, "i:e:sh")) != -1){
+		switch(result){
+			case 'i':
+				input(arch, optarg);
+				break;
+			case 'e':
+				extract(arch, optarg);
+				break;
+			case 's':
+				stats(arch);
+				break;
+			case 'h':
+				help();
+				break;
+			default:
+				help();
+				break;
+		}
+	}
+	return 0;
+
+}
+
