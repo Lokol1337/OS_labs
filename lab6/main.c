bd1ec3f7d576fcb6e2c0ef665bf4913eec990e4c
Lokol1337 <drejfeld@gmail.com>
2021-12-15 11:29:55 +0300

    lab6

--- lab6/main.c
+++ lab6/main.c
@@ -4,26 +4,24 @@
 #include <unistd.h>
 
 static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 int num = 0;
 void* funW(void* data){
-	int i = 0;
 	while(1){
-		i += 1;
 		pthread_mutex_lock(&mutex);
 		num = num + 1;
+		pthread_cond_broadcast(&cond);
 		pthread_mutex_unlock(&mutex);
 		sleep(1);
 	}
 }
 
 void* funR(void* data){
-	int i = 0;
 	while(1){
-		i += 1;
 		pthread_mutex_lock(&mutex);
+		pthread_cond_wait(&cond,&mutex);
 		printf("thread id: %lu\nnum status: %d\n",pthread_self(), num);
 		pthread_mutex_unlock(&mutex);
-		sleep(1);
 	}
 	pthread_exit(NULL);
 	
@@ -34,14 +32,17 @@ int main(int argc, char** argv){
 		
 	pthread_t w,r[10];
 	
-	pthread_create(&w, NULL, funW, NULL);
 
 	for(int i = 0; i < 10; i++){
 		pthread_create(&r[i], NULL, funR, NULL);
 	}
-	
+
+	pthread_create(&w, NULL, funW, NULL);
+
 	pthread_mutex_destroy(&mutex);
-	pthread_join(w,NULL);		
+	pthread_cond_destroy(&cond);
+	pthread_join(w,NULL);	
+	
 	return 0;
 
 }
